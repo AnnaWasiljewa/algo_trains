a3cf54ffe9aaef3549fb91b45e833280a54821c9
Anna Wasiljewa <awasiljewa@gmail.com>
2026-02-23 13:44:40 +0300

    Update 11A.cpp

--- 11A.cpp
+++ 11A.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    int a, b;
+    string s;
+	cin >> a >> b;
+	cin >> s;
+	if (s == "freeze")
+	    cout << min(a, b) << endl;
+	else if (s == "heat")
+	    cout << max(a, b);
+	else if (s == "auto")
+	    cout << b;
+	else if (s == "fan")
+	    cout << a;
+	else
+	    cout << "error";
+	return 0;
+}
